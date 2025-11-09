cbe529110535848d4c1d541325f44cb035651d3a
Getuar333 <getuar.gaming@gmail.com>
2025-11-09 13:05:33 +0100

    Update api.js

--- frontend/api.js
+++ frontend/api.js
@@ -1,1 +1,19 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://localhost:7189/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+API.interceptors.request.use((config) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (user?.token) {
+    config.headers.Authorization = `Bearer ${user.token}`;
+  }
+  return config;
+});
+
+export default API;
 
